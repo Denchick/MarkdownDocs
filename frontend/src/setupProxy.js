c68c363db1a82faef7a477f20eb3e3310dc0e78c
Volkov Denis <volkov.denis.dev@yandex.ru>
2019-12-25 18:55:48 +0500

    вдогонку

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -2,10 +2,6 @@ const proxy = require('http-proxy-middleware');
 module.exports = function(app) {
     app.use(proxy('/api', { 
         target: 'http://localhost:8228/',
-        headers: {
-            auth: 'secret',
-            userId: 'f63e3113-9583-47ee-9a8e-809f9077e6f7'
-        },
         ws: false
     }));
 }
